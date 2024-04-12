82e70dbac87fa975bbaeb796f61487f7339e105f
Wouter de Vos <309331+wearethefoos@users.noreply.github.com>
2024-04-12 16:11:28 +0200

    fix: Middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
-import { auth } from "@/lib/auth";
 
 export function middleware(request: NextRequest) {
-  let response = NextResponse.next({ request: auth.request });
+  let response = NextResponse.next();
   response.headers.set("x-url", request.url);
   response.headers.set("x-pathname", request.nextUrl.pathname);
   return response;
